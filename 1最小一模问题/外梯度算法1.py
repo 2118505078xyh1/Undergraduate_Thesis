ad36a670cdec02396a226965c5d749d7544a5955
2118505078xyh1 <96534106+2118505078xyh1@users.noreply.github.com>
2024-05-13 17:13:04 +0800

    Add files via upload

---
+++ 1最小一模问题/外梯度算法1.py
@@ -0,0 +1,60 @@
+# 算法1 by xyh
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+
+A = np.random.normal(0.0, 1.0, (2000, 3000))
+x_ture = np.zeros((3000, 1))
+for _ in range(300):
+    x_ture[_ * 10] = 1
+    x_ture[_ * 10 + 5] = -1
+b = np.dot(A, x_ture)
+x = np.ones((3000, 1))
+y0 = np.zeros((2000, 1))
+beta = 1
+v = 0.9
+mu = 0.5
+k = 0
+p = []
+t1 = time.time()  # begin time
+while True:
+    x1 = np.max(np.hstack((x - beta * np.dot(A.transpose(), y0), np.zeros((3000, 1)))), axis=1)
+    x1 = x1.reshape(3000, 1)
+    y1 = y0 - beta * (-np.dot(A, x) + b)
+    r = beta * np.linalg.norm(
+        np.vstack((np.dot(A.transpose(), y0 - y1), np.dot(A, x - x1)))) / np.linalg.norm(
+        np.vstack((x - x1, y0 - y1)))
+    while r > v:
+        beta = (2 / 3) * beta * min(1, 1 / r)
+        x1 = np.max(np.hstack((x - beta * np.dot(A.transpose(), y0), np.zeros((3000, 1)))), axis=1)
+        x1 = x1.reshape(3000, 1)
+        y1 = y0 - beta * (-np.dot(A, x) +b)
+        r = beta * np.linalg.norm(
+            np.vstack((np.dot(A.transpose(), y0 - y1), np.dot(A, x - x1)))) / np.linalg.norm(
+            np.vstack((x - x1, y0 - y1)))
+    x2 = np.max(np.hstack((x - beta * np.dot(A.transpose() , y1), np.zeros((3000, 1)))), axis=1)
+    x2 = x2.reshape(3000, 1)
+    y0 = y0 - beta * (-np.dot(A, x1) + b)
+    if r <= mu:
+        beta = 1.5 * beta
+    p.append(np.linalg.norm(x2 - x))
+#    print(beta, r, k, np.linalg.norm(x2 - x), )
+    if k == 500 or k == 1000:
+        t2 = time.time()  # end time
+        t = t2 - t1  # compute running time
+ #       print(t)
+    if np.linalg.norm(x2 - x) < 0.001 or k > 100:
+        break
+    k = k + 1
+    x = x2
+t2 = time.time()  # end time
+t = t2 - t1  # compute running time
+#print(t)
+plt.plot(p)
+# 添加标题和轴标签
+plt.xlabel('number of iterations')
+plt.ylabel('||xk-xk-1||2')
+# 显示网格
+plt.grid(True)
+# 显示图像
+plt.show()
---
+++ 1最小一模问题/投影收缩算法2.py
@@ -0,0 +1,80 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+
+
+def fun2(x, y0, beta, v, mu, gama,k0):
+    k = 0
+    p = []
+    t1 = time.time()  # begin time
+    while True:
+        x1 = np.max(np.hstack((x - beta * np.dot(A.transpose(), y0), np.zeros((3000, 1)))), axis=1)
+        x1 = x1.reshape(3000, 1)
+        y1 = y0 - beta * (-np.dot(A, x) + b)
+        r = beta * np.linalg.norm(
+            np.vstack((np.dot(A.transpose(), y0 - y1), np.dot(A, x - x1)))) / np.linalg.norm(
+            np.vstack((x - x1, y0 - y1)))
+        while r > v:
+            beta = (2 / 3) * beta * min(1, 1 / r)
+            x1 = np.max(np.hstack((x - beta * np.dot(A.transpose(), y0), np.zeros((3000, 1)))), axis=1)
+            x1 = x1.reshape(3000, 1)
+            y1 = y0 - beta * (-np.dot(A, x) + b)
+            r = beta * np.linalg.norm(
+                np.vstack((np.dot(A.transpose(), y0 - y1), np.dot(A, x - x1)))) / np.linalg.norm(
+                np.vstack((x - x1, y0 - y1)))
+        alpha = np.linalg.norm(np.vstack((x - x1, y0 - y1))) ** 2 / np.linalg.norm(np.vstack((x - x1 - beta * (
+            np.dot(A.transpose(), y0 - y1)), y0 - y1 + beta * (np.dot(A, x - x1))))) ** 2
+        x2 = x - gama * alpha * (x - x1 - beta * (np.dot(A.transpose(), y0 - y1)))
+        x2 = x2.reshape(3000, 1)
+        y2 = y0 - gama * alpha * (y0 - y1 + beta * (np.dot(A, x - x1)))
+        if r <= mu:
+            beta = 1.5 * beta
+        p.append(np.linalg.norm(x2 - x))
+        if k == 500 or k == 1000:
+            t2 = time.time()  # end time
+            t = t2 - t1  # compute running time
+ #           print(k, t)
+        if np.linalg.norm(x2 - x) < 0.001 or k >k0:
+            t2 = time.time()  # end time
+            t = t2 - t1  # compute running time
+            return gama, k, np.linalg.norm(x2 - x), t
+        k = k + 1
+        x = x2
+        y0 = y2
+    plt.plot(p)
+    # 添加标题和轴标签
+    plt.xlabel('number of iterations')
+    plt.ylabel(f"||xk-xk-1||2,gama = {gama} ")
+    # 显示网格
+    plt.grid(True)
+    # 显示图像
+    plt.show()
+
+
+if __name__ == "__main__":
+    A = np.random.normal(0.0, 1.0, (2000, 3000))
+    x_ture = np.zeros((3000, 1))
+    for _ in range(300):
+        x_ture[_ * 10] = 1
+        x_ture[_ * 10 + 5] = -1
+    b = np.dot(A, x_ture)
+    x = np.ones((3000, 1))
+    y0 = np.zeros((2000, 1))
+    beta = 1
+    v = 0.9
+    mu = 0.5
+    k0=100
+    l_gama=[]
+    l_t=[]
+    l_k=[]
+    for gama in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3]:
+        gama, k, norm2, t = fun2(x, y0, beta, v, mu, gama,k0)
+        l_gama.append(gama)
+        l_t.append(t)
+        l_k.append(k)
+        print(gama, k, norm2, t)
+ #   print(l_gama,l_k,l_t)
+    plt.plot(l_gama,l_k)
+    plt.show()
+    plt.plot(l_gama, l_t)
+    plt.show()
